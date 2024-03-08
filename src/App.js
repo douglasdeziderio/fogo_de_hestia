de457ae3097c0a8ca46b761f3b5537b5da0e2117
Douglas Deziderio <douglas_alex96@hotmail.com>
2024-03-08 10:21:22 -0300

    projeto pessoal

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Corpo from './components/Corpo';
+import Topo from './components/Topo';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Topo/>
+      <Corpo/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Corpo/index.js
@@ -0,0 +1,19 @@
+import './Corpo.css'
+
+function Corpo(){
+    return(
+        <div className="corpo">
+            <div className='box'>
+                <div>
+
+                </div>
+
+                <div>
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Corpo
